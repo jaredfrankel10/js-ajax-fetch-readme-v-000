a93dd779ae548240d274864bba85c5055ad16378
jared frankel <jaredfrankel10@gmail.com>
2018-02-02 15:26:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+   .then((response) => {
+     return response.json()
+   })
+   .then((json) => {
+     console.log(json);
+   });
